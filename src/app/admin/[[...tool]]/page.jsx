bcf1048b5ea0971178d9711de77e3ed79b337c48
Kushal Gohil <2002kushalgohil@gmail.com>
2024-08-05 16:38:15 +0530

    Fixed Admin Panel Structure

--- src/app/admin/[[...tool]]/page.jsx
+++ src/app/admin/[[...tool]]/page.jsx
@@ -6,5 +6,11 @@ export const dynamic = "force-static";
 export { metadata, viewport } from "next-sanity/studio";
 
 export default function StudioPage() {
-  return <NextStudio config={config} />;
+  return (
+    <html>
+      <body>
+        <NextStudio config={config} />
+      </body>
+    </html>
+  );
 }
