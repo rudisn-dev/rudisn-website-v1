f10c1581c7a4754f19e6aa4c591758d6cef1dc0f
Kushal Gohil <2002kushalgohil@gmail.com>
2024-08-05 16:32:36 +0530

    Fixed Layout Issue for not found

--- src/app/(client)/layout.js
+++ src/app/(client)/layout.js
@@ -1,35 +1,5 @@
-"use client";
-import "swiper/css";
-import "swiper/css/effect-fade";
-import "remixicon/fonts/remixicon.css";
-import "../../../public/assets/css/bootstrap.min.css";
-import "../../../public/assets/css/style.css";
-import useMagneticHover from "@/hooks/useMagneticHover";
-import ScrollTopBtn from "@/utils/ScrollTopBtn";
-import { useEffect } from "react";
-import ThemeSwitch from "@/components/common/Theme";
-import { SpeedInsights } from "@vercel/speed-insights/next";
-import { GoogleAnalytics } from "@next/third-parties/google";
-import NavBar from "@/components/header/NavBar";
-import Footer from "@/components/Footer/Footer";
+import RootLayout from "@/components/layouts/RootLayout";
 
-export default function RootLayout({ children }) {
-  useMagneticHover();
-  useEffect(() => {
-    require("bootstrap/dist/js/bootstrap.bundle.min.js");
-  }, []);
-
-  return (
-    <html lang="en">
-      <body>
-        <NavBar />
-        {children}
-        <Footer />
-        <ThemeSwitch />
-        <ScrollTopBtn />
-        <SpeedInsights />
-      </body>
-      <GoogleAnalytics gaId="G-PP6G83BBBL" />
-    </html>
-  );
+export default function Layout({ children }) {
+  return <RootLayout>{children}</RootLayout>;
 }
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,3 @@
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+export default function Layout({ children }) {
+  return children;
 }
