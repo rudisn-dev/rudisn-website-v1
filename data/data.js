33293cb67125f411e78c53ecbb887adc8f9b460f
Kushal Gohil <2002kushalgohil@gmail.com>
2024-08-28 11:53:40 +0530

    Added Team member photo

--- data/data.js
+++ data/data.js
@@ -1045,7 +1045,7 @@ export const allTeamMembers = [
   {
     name: "Sachin",
     designation: "Software Engineer",
-    imageSrc: "/assets/images/our-team/members/team-member.png",
+    imageSrc: "/assets/images/our-team/members/sachin.png",
   },
   {
     name: "Atharva",
