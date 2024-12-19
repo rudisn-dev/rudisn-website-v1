fd6ecd39ef0af73e6aca1d1e124fe0434dc1e358
Kushal Gohil <2002kushalgohil@gmail.com>
2024-12-19 10:09:43 +0530

    Added Chat BOT

--- src/app/(client)/layout.js
+++ src/app/(client)/layout.js
@@ -1,5 +1,24 @@
 import RootLayout from "@/components/layouts/RootLayout";
+import Script from "next/script";
 
 export default function Layout({ children }) {
-  return <RootLayout>{children}</RootLayout>;
+  return (
+    <RootLayout>
+      <Script id="embedded-chatbot-config" strategy="afterInteractive">
+        {`
+          window.embeddedChatbotConfig = {
+            chatbotId: "Z4yXsh8r619FGdU1hDYZ3",
+            domain: "www.chatbase.co"
+          };
+        `}
+      </Script>
+      <Script
+        src="https://www.chatbase.co/embed.min.js"
+        strategy="afterInteractive"
+        data-chatbot-id="Z4yXsh8r619FGdU1hDYZ3"
+        data-domain="www.chatbase.co"
+      />
+      {children}
+    </RootLayout>
+  );
 }
